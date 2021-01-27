27219efdef9a5bd5435f87508c0131189e08a91b
Sebastian Gode <sebastian.gode@telekom.de>
2021-01-27 09:49:20 +0000

    Initial commit

---
+++ removemodulename.py
@@ -0,0 +1,30 @@
+import os
+
+# Initial facts
+charnumber = 0
+moduledirold = 'modules/'
+moduledirnew = 'modules2/'
+
+
+
+
+for filename in os.listdir(moduledirold):
+    # Exclude index.rst as it has not the issue
+    if filename != 'index.rst':
+        with open((moduledirold+filename), 'r') as rstfile:
+            content = rstfile.readlines()
+            # Search for the line below the Title Name through '+++'
+            linenumber = [x for x in range(len(content)) if '+++' in content[x].lower()]
+            for i in content[linenumber[0]-1]:
+                charnumber = charnumber+1
+                # Do the Magic and search for '--' and save newline with everything behind the --
+                if content[linenumber[0]-1][charnumber] == '-' and content[linenumber[0]-1][charnumber+1] == '-':
+                    newline = content[linenumber[0]-1][(charnumber+3):]
+                    break 
+            # Now change the file content Title with the newline 
+            content[linenumber[0]-1] = newline
+            charnumber=0
+            # Write that to a new file
+            with open((moduledirnew+filename), 'a') as rstfileout:
+                for i in content:
+                    rstfileout.write(i)
